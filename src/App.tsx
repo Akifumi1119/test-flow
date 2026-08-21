185eba81f8132ec8c95a14598ad9110ecd6eb4ff
Akifumi1119 <akifumi.doi1119@gmail.com>
2026-08-21 15:14:53 +0900

    20260821 デプロイ準備

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router-dom'
+import { HashRouter, Navigate, Outlet, Route, Routes } from 'react-router-dom'
 import { AppLayout } from './layouts/AppLayout'
 import { DashboardPage } from './pages/DashboardPage'
 import { LoginPage } from './pages/LoginPage'
@@ -13,7 +13,7 @@ function PrivateRoute() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         {/* 認証不要 */}
         <Route path="/login" element={<LoginPage />} />
@@ -28,7 +28,7 @@ function App() {
           </Route>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
